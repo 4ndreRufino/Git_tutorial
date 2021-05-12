96ad1428240ed954b70137a691cf4a3be07a7d0e
Andre Luiz Rufino Cordeiro <andrecordeiro@petrobras.com.br>
2021-05-12 08:18:37 -0300

    Criada a classe Vector
    Foi criada a classe Vector no arquivo

--- vector.py
+++ vector.py
@@ -1,7 +1,15 @@
-"""Módulo vector.py Addition:
+"""Módulo vector.py
+Addition:
 >>> v1 = Vector(2, 4)
 >>> v2 = Vector(2, 1)
->>> v1 + v2 Vector(4, 5) Absolute value::
+>>> v1 + v2 Vector(4, 5)
+Absolute value::
 >>> v = Vector(3, 4)
->>> abs(v) 5.0 Scalar multiplication::
->>> v * 3 Vector(9, 12) >>> abs(v * 3) 15.0 """
+>>> abs(v) 5.0
+Scalar multiplication::
+>>> v * 3 Vector(9, 12)
+>>> abs(v * 3)
+15.0 """
+
+class Vector:
+    pass
